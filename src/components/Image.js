517bd30c961829eb9bd812f635bc7a3aa3e8bc2a
kochewww <vladusta2@gmail.com>
2020-01-10 11:05:59 +0300

    images icons have been added on hover

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Image({ className, img }) {
+  const [hovered, setIsHovered] = useState(false);
+  const favIcon = hovered && <i className="ri-heart-line favorite"></i>;
+  const plusIcon = hovered && <i className="ri-add-circle-line cart"></i>;
+  console.log(hovered);
   return (
     <div className={`${className} image-container`}>
-      <img src={img.url} alt="" className="image-grid" />
+      <img
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+        src={img.url}
+        alt=""
+        className="image-grid"
+      />
+      {favIcon}
+      {plusIcon}
     </div>
   );
 }
