5b98b7cf8c7619ff6559665212508038cf174699
kochewww <vladusta2@gmail.com>
2020-01-13 12:07:32 +0300

    cart icon changeabilty added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
-
+import { Context } from "../Context";
 function Header() {
+  const { cartPhotos } = useContext(Context);
+  const cartIconClassName =
+    cartPhotos.length > 0 ? "ri-shopping-cart-fill" : "ri-shopping-cart-line";
   return (
     <header>
       <Link to="/">
         <h2>Pic Some</h2>
       </Link>
       <Link to="/cart">
-        <i className="ri-shopping-cart-line ri-fw ri-2x"></i>
+        <i className={`${cartIconClassName} ri-fw ri-2x`}></i>
       </Link>
     </header>
   );
